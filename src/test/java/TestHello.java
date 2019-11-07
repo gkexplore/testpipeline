28ca05351ae2329ea5de6de20998d382edf9c035
Karthik Ganesan <gkexplore@gmail.com>
2019-11-06 21:44:07 -0700

    Initial commit

---
+++ src/test/java/TestHello.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class TestHello {
+
+    @Test
+    public void testJenkinsPipeline(){
+        System.out.println("Hello pipeline");
+        assertTrue(true);
+    }
+}
